95562b6127ce9bf4a5ce6e9ad6e2ee8f2451262f
Alfonso García Martínez <alfonsogmar@gmail.com>
2024-11-18 19:03:49 +0000

    ADD source tests, new packages and UPDATE promos model

--- models/staging/sql_server_dbo/stg_sql_server_dbo__promos.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__promos.sql
@@ -11,18 +11,18 @@ WITH src_promos AS (
 
 renamed_casted_hashed AS (
     SELECT
-        md5(promo_id) AS promo_id,
+        {{ dbt_utils.generate_surrogate_key(['promo_id']) }} AS promo_id,
         promo_id AS promo_desc,
         to_numeric((discount/100), 5, 2) AS discount,
-        status,
-        _fivetran_synced AS load_date
+        (status='active') AS is_active,
+        {{ dbt_date.convert_timezone( '_fivetran_synced', "Europe/Kiev", "UTC") }} AS load_date
     FROM src_promos
     UNION ALL
     SELECT 
-        md5('') AS promo_id,
+        md5('No promo') AS promo_id,
         'No promo' AS promo_desc,
         0.00 AS discount,
-        'Inactive' AS status,
+        False AS is_active,
         Null AS load_date
     )
 
