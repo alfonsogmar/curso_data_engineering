dc0fc549ed893b91b6e990043a72f4d6bac9377d
Alfonso García Martínez <alfonsogmar@gmail.com>
2024-11-21 19:00:08 +0000

    ADD users gold model metadata and UPDATE model

--- models/marts/core/users.sql
+++ models/marts/core/users.sql
@@ -7,7 +7,18 @@
 WITH stg_users AS (
     SELECT * 
     FROM {{ ref('stg_sql_server_dbo__users') }}
+),
+
+selected_fields AS (
+    SELECT
+        user_id,
+        first_name,
+        last_name,
+        email,
+        address_id, -- mejor hacer join con tabla de addresses
+        phone_number
+    FROM stg_users
 )
 
 
-SELECT * FROM stg_users
+SELECT * FROM selected_fields
