2919beb7301f8c551a0884caefe95ae5bab22b32
Alfonso García Martínez <alfonsogmar@gmail.com>
2024-11-26 10:41:54 +0000

    Rename columns in stg addresses model

--- models/staging/sql_server_dbo/stg_sql_server_dbo__addresses.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__addresses.sql
@@ -11,11 +11,11 @@ WITH src_addresses AS (
 
 renamed_casted AS (
     SELECT
-        address_id::VARCHAR,
-        address::VARCHAR,
-        zipcode::VARCHAR,
-        country::VARCHAR,
-        state::VARCHAR,
+        address_id::VARCHAR AS address_id,
+        address::VARCHAR AS address,
+        zipcode::VARCHAR AS zipcode,
+        country::VARCHAR AS country,
+        state::VARCHAR AS state,
         CONVERT_TIMEZONE('UTC', _fivetran_synced) AS load_date_utc
     FROM src_addresses
     )
