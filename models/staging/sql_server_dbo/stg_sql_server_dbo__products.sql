556e572baf21e7d4691796257c03b4628a34ad02
Alfonso García Martínez <alfonsogmar@gmail.com>
2024-11-27 16:45:06 +0000

    ADD products stg model

---
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__products.sql
@@ -0,0 +1,21 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+
+WITH src_products AS (
+    SELECT * 
+    FROM {{source('sql_server_dbo','products') }}
+),
+
+casted_renamed AS(
+    SELECT
+        product_id::VARCHAR(40) AS product_id,
+        price::NUMERIC(5,2) AS price,
+        name::VARCHAR(50) AS name,
+	    inventory::INT AS inventory
+    FROM src_order_items
+)
+
+SELECT * FROM casted_with_surrogate_key
