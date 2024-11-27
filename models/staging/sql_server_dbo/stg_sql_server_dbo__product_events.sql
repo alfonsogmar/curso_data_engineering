2b02e2f45cf733ebd6c0a2da29f2acefcdb35b23
Alfonso García Martínez <alfonsogmar@gmail.com>
2024-11-27 18:13:57 +0000

    ADD more stg models

---
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__product_events.sql
@@ -0,0 +1,20 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+
+WITH base_events AS (
+    SELECT * 
+    FROM {{ ref('base_sql_server_dbo__events') }}
+),
+
+product_event_fields AS (
+    SELECT
+        event_id,
+        product_id,
+    FROM base_events
+    WHERE product_id != ''
+)
+
+SELECT * FROM product_event_fields
