2fc4a67c04707c0710975935187ae33d75407bff
Alfonso García Martínez <alfonsogmar@gmail.com>
2024-11-29 18:03:57 +0000

    UPDATE singular test to refactor WHERE clause logic

--- tests/creation_date_is_prior_to_delivery_dates.sql
+++ tests/creation_date_is_prior_to_delivery_dates.sql
@@ -2,18 +2,12 @@ SELECT * FROM {{ ref('stg_sql_server_dbo__orders') }}
 WHERE
     NOT (
         delivered_at_utc IS Null
-        OR (
-            delivered_at_utc IS NOT Null
-            AND
-            created_at_utc <= delivered_at_utc
-        )
+        OR 
+        created_at_utc <= delivered_at_utc
     )
     OR
     NOT (
         estimated_delivery_at_utc IS Null
-        OR (
-            estimated_delivery_at_utc IS NOT Null
-            AND
-            created_at_utc <= estimated_delivery_at_utc
-        )
+        OR 
+        created_at_utc <= estimated_delivery_at_utc
     )
