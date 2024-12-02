c6c4305669d1807e5e6aa353faa7643aa0f04c97
Alfonso García Martínez <alfonsogmar@gmail.com>
2024-12-02 16:24:19 +0000

    ADD historical data fields to stg model

--- models/staging/sql_server_dbo/stg_sql_server_dbo__orders_evolution.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__orders_evolution.sql
@@ -9,12 +9,16 @@ WITH order_snapshot AS (
     FROM {{ref('order_snapshot') }}
 ),
 
-order_fields AS(
+order_evolution_fields AS(
     SELECT
         order_id,
         status,
-        delivered_at_utc
+        delivered_at_utc,
+        load_date_utc,
+        dbt_valid_from AS valid_from,
+        dbt_valid_to AS valid_to,
+        (dbt_valid_to IS NULL) AS is_current
     FROM order_snapshot
 )
 
-SELECT * FROM order_fields
+SELECT * FROM order_evolution_fields
