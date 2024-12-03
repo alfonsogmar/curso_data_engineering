58d2f2f4d7148b205034bfeb7517653039cc12d1
Alfonso García Martínez <alfonsogmar@gmail.com>
2024-12-03 22:09:44 +0000

    UPDATE orders, events and order_items models to make them incremental

--- models/staging/sql_server_dbo/base/base_sql_server_dbo__events.sql
+++ models/staging/sql_server_dbo/base/base_sql_server_dbo__events.sql
@@ -1,6 +1,8 @@
 {{
   config(
-    materialized='view'
+    materialized='incremental',
+    incremental_strategy='merge',
+    unique_key='event_id'
   )
 }}
 
@@ -15,13 +17,19 @@ renamed_casted AS (
         {{dbt_utils.generate_surrogate_key(['event_type'])}} AS event_type_id,
         event_type::VARCHAR(20) AS event_type,
         order_id::VARCHAR(40) AS order_id,
-        page_url::VARCHAR(40) AS page_url,
+        page_url::VARCHAR(150) AS page_url,
         product_id::VARCHAR(40) AS product_id,
         session_id::VARCHAR(40) AS session_id,
         user_id::VARCHAR(40) AS user_id,
         CONVERT_TIMEZONE('UTC', created_at) AS created_at_utc,
         CONVERT_TIMEZONE('UTC', _fivetran_synced) AS load_date_utc
     FROM src_events
+
+    {% if is_incremental() %}
+
+    where _fivetran_synced >= (select coalesce(max(_fivetran_synced),'1900-01-01') from {{ this }} )
+
+    {% endif %}
 )
 
 SELECT * FROM renamed_casted
--- models/staging/sql_server_dbo/stg_sql_server_dbo__order_items.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__order_items.sql
@@ -1,6 +1,8 @@
 {{
   config(
-    materialized='view'
+    materialized='incremental',
+    incremental_strategy='merge',
+    unique_key=['order_id', 'product_id']
   )
 }}
 
@@ -14,8 +16,15 @@ casted_with_surrogate_key AS(
         {{ dbt_utils.generate_surrogate_key(['order_id','product_id']) }} AS order_item_id,
         order_id::VARCHAR(40) AS order_id,
         product_id::VARCHAR(40) AS product_id,
-        quantity::INT AS quantity
+        quantity::INT AS quantity,
+        CONVERT_TIMEZONE('UTC', _fivetran_synced) AS load_date_utc
     FROM src_order_items
+
+    {% if is_incremental() %}
+
+    where _fivetran_synced >= (select coalesce(max(_fivetran_synced),'1900-01-01') from {{ this }} )
+
+    {% endif %}
 )
 
 SELECT * FROM casted_with_surrogate_key
